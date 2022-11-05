8dcbcad74019c212f508457732faf0c07136b9d2
Nawodya Jayalath <nawodyaonline@gmail.com>
2022-11-05 17:35:15 +0530

    Add update function

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Employee = ({ employee, deleteEmployee }) => {
+    const navigate =  useNavigate();
+    const editEmployee = (e, id) => {
+        e.preventDefault();
+        navigate(`/editEmployee/${id}`)
+    }
+
   return (
         <tr>
                             <td className="text-left px-6 py-4 whitespace-nowrap">
@@ -13,8 +20,10 @@ const Employee = ({ employee, deleteEmployee }) => {
                                 <div className="text-sm text-gray-500">{employee.email}</div>
                             </td>
                             <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
-                                <a href="#" className="text-indigo-600 hover:text-indigo-800 px-4">Edit</a>
-                                <a href="#"
+                                <a href='*'
+                                onClick={(e) => editEmployee(e, employee.id)}
+                                className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer">Edit</a>
+                                <a href='*'
                                 onClick={(e)=> deleteEmployee(e, employee.id)}
                                 className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer">Delete</a>
                             </td>
---
+++ src/components/UpdateEmployee.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom';
+import EmployeeService from '../services/EmployeeService';
+
+const UpdateEmployee = () => {
+    const {id} = useParams();
+
+    const navigate = useNavigate();
+
+    const [employee, setEmployee] = useState({
+        id: id,
+        firstName: "",
+        lastName: "",
+        emailId: ""
+      })
+    
+    const updateEmployee = (e) => {
+        e.preventDefault();
+        EmployeeService.updateEmployee(employee, id)
+            .then((res) => {
+                navigate("/employeeList")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setEmployee({...employee, [e.target.name]: value});
+    }
+
+    const reset = (e) => {
+        e.preventDefault(); 
+        setEmployee({
+          id: "",
+          firstName: "",
+          lastName: "",
+          emailId: ""
+        })
+      }
+    
+    useEffect(() => {
+      const fetchData = async () => {
+        try{
+            const response = await EmployeeService.getEmployeeById(id);
+            setEmployee(response.data );
+        } catch(err) {
+            console.log(err)
+        }
+      };
+      fetchData();
+
+    }, [])
+    
+
+  return (
+    <div className="flex max-w-2xl mx-auto shadow border-b">
+        <div className="px-8 py-8">
+            <div className="font-thin text-2xl tracking-wider">
+                <h1>Edit Employee</h1>
+            </div>
+            <div className="items-center justify-center h-14 w-full my-4">
+                <label className="block text-gray-600 text-sm font-normal" htmlFor="">First Name</label>
+                <input name="firstName"
+                onChange={(e) => handleChange(e)}
+                value={employee.firstName} className="h-10 w-96 border mt-2 px-2 py-2" type="text" />
+            </div>
+            <div className="items-center justify-center h-14 w-full my-4">
+                <label className="block text-gray-600 text-sm font-normal" htmlFor="">Last Name</label>
+                <input name='lastName'
+                onChange={(e) => handleChange(e)}
+                value={employee.lastName} className="h-10 w-96 border mt-2 px-2 py-2" type="text" />
+            </div>
+            <div className="items-center justify-center h-14 w-full my-4">
+                <label className="block text-gray-600 text-sm font-normal" htmlFor="">Email</label>
+                <input name='emailId'
+                onChange={(e) => handleChange(e)}
+                value={employee.emailId} className="h-10 w-96 border mt-2 px-2 py-2" type="email" />
+            </div>
+
+            <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
+              <button onClick={updateEmployee} className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6">Update</button>
+              <button
+              onClick={() => navigate("/employeeList")}
+              className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6">Cancel</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UpdateEmployee
